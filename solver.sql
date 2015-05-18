534f8bad2cabbffe8ceac33794b51483721e285f
Pierre Ducroquet <pinaraf@pinaraf.info>
2015-05-19 01:31:21 +0200

    Simplify the code by 'constifying' more stuff

--- solver.sql
+++ solver.sql
@@ -1,58 +1,42 @@
 ﻿WITH
 	-- A few 'constants' to start with
 	digits as (select d from generate_series(1, 9) d),
-	positions as (select p from generate_series(1, 81) p),
-
-	-- Now the recursive part
---	RECURSIVE board(b, recurs_over) as (
---
---	)
-	board AS (select board b, false as recurs_over from sudokus limit 1),
-	associated_values as (
-		select
-			p, 
-			array_cat(
-				array_cat(
-					array_agg(b[d1.d + 9 * ((p - 1) / 9)]), -- board_row,
-					array_agg(b[1 + (d1.d - 1) * 9 + (p - 1) % 9]) -- board_col,
-				),
-				array_agg(b[
-					-- The block iterator
-					((d1.d-1)/ 3) * 9 + (d1.d - 1) % 3 +
-					-- Find the block base number from the position
-					-- block base number : (((p - 1) / 27) * 3 + ((p - 1) % 9) / 3)
-					((((p - 1) / 27) * 3 + ((p - 1) % 9) / 3) / 3) * 27 + (((p - 1) / 27) * 3 + ((p - 1) % 9) / 3) % 3 * 3  +
-					1]
-				) -- board_block
-			) as neighb
-		from 
-			positions, 
-			digits d1, 
-			board
-		where b[p] = 0
-		group by p order by p
+	positions as (
+		select 
+			p,
+			array_agg(9 * ((p - 1) / 9) + d) as row_ps,
+			array_agg(1 + ((p - 1) % 9) + (d - 1) * 9) as col_ps,
+			array_agg(((d-1)/ 3) * 9 + (d - 1) % 3 + ((((p - 1) / 27) * 3 + ((p - 1) % 9) / 3) / 3) * 27 + (((p - 1) / 27) * 3 + ((p - 1) % 9) / 3) % 3 * 3  + 1) as block_ps
+		from generate_series(1, 81) p, digits
+		group by 1
 	),
+	dependencies (p, dep) as (
+		select
+			p,
+			unnest(row_ps | col_ps | block_ps) as dep
+		from positions order by 1, 2
+	),
+
+	
+	-- Now import the data
+	board AS (select board b, false as recurs_over from sudokus limit 1),
+	-- And mix everything up
 	possibilities as (
 		select
-			p, 
-			array_agg(d) as vals
-		from
-			associated_values,
-			digits
+			p,
+			array_agg(d) - uniq(sort(array_agg(b[dep]))) as vals
+		from dependencies, board, digits
 		where
-			not(d = any(neighb))
-		group by p order by p
+			b[p] = 0 and b[dep] <> 0
+		group by p
+		having array_length(uniq(sort(array_agg(b[dep]))), 1) = 8
+		order by p
 	),
 	new_board as (
-		select 
-			array_agg(case when array_length(vals, 1) = 1 then vals[1] else b[p.p] end) as board,
-			case when array_agg(case when array_length(vals, 1) = 1 then vals[1] else b[p.p] end) = b then true else false end as recurse_over
-			
-		from
-			board, 
-			positions p
-			left join possibilities on possibilities.p = p.p
-		group by b
-		order by 1
+		select
+			array_agg(coalesce(possibilities.vals[1], b[positions.p]) order by positions.p) as board,
+			false as recurse_over
+		from board join positions on true left join possibilities on possibilities.p = positions.p 
 	)
-select * from new_board union all select * from board;
+select * from new_board;
+
