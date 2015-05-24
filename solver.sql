c49a168ec4cb33595641e430fb6de9e9b1d5e817
Pierre Ducroquet <pinaraf@pinaraf.info>
2015-05-24 12:13:11 +0200

    A new array-less version, still not recursive

--- solver.sql
+++ solver.sql
@@ -1,42 +1,45 @@
 ﻿WITH
-	-- A few 'constants' to start with
-	digits as (select d from generate_series(1, 9) d),
-	positions as (
-		select 
-			p,
-			array_agg(9 * ((p - 1) / 9) + d) as row_ps,
-			array_agg(1 + ((p - 1) % 9) + (d - 1) * 9) as col_ps,
-			array_agg(((d-1)/ 3) * 9 + (d - 1) % 3 + ((((p - 1) / 27) * 3 + ((p - 1) % 9) / 3) / 3) * 27 + (((p - 1) / 27) * 3 + ((p - 1) % 9) / 3) % 3 * 3  + 1) as block_ps
-		from generate_series(1, 81) p, digits
-		group by 1
+	board_pieces AS (
+		SELECT
+			1 as board_id,	-- TODO...
+			position,
+			board[position] as value
+		FROM sudokus, generate_series(1, 81) position ORDER BY 1, 2
 	),
-	dependencies (p, dep) as (
-		select
-			p,
-			unnest(row_ps | col_ps | block_ps) as dep
-		from positions order by 1, 2
+	digits as (
+		SELECT d FROM generate_series(1, 9) d
 	),
-
-	
-	-- Now import the data
-	board AS (select board b, false as recurs_over from sudokus limit 1),
-	-- And mix everything up
-	possibilities as (
+	dependencies as (
 		select
-			p,
-			array_agg(d) - uniq(sort(array_agg(b[dep]))) as vals
-		from dependencies, board, digits
-		where
-			b[p] = 0 and b[dep] <> 0
-		group by p
-		having array_length(uniq(sort(array_agg(b[dep]))), 1) = 8
-		order by p
+			distinct on (position, dependency)
+			p as position,
+			case
+				when direction = 'row' then
+					9 * ((p - 1) / 9) + d
+				when direction = 'col' then
+					1 + ((p - 1) % 9) + (d - 1) * 9
+				else
+					((d-1)/ 3) * 9 + (d - 1) % 3 + ((((p - 1) / 27) * 3 + ((p - 1) % 9) / 3) / 3) * 27 + (((p - 1) / 27) * 3 + ((p - 1) % 9) / 3) % 3 * 3  + 1
+			end as dependency
+		from generate_series(1, 81) p, digits, (values ('row'), ('col'), ('block') ) as d(direction)
+		order by 1, 2
 	),
-	new_board as (
-		select
-			array_agg(coalesce(possibilities.vals[1], b[positions.p]) order by positions.p) as board,
-			false as recurse_over
-		from board join positions on true left join possibilities on possibilities.p = positions.p 
+	unknowns as (
+		SELECT
+			bp.position,
+			max(digits.d)
+		FROM
+			board_pieces bp, digits
+		WHERE
+			bp.value = 0
+			AND NOT EXISTS(
+				SELECT *
+				FROM
+					dependencies deps
+				JOIN board_pieces bp_dep ON bp_dep.position = deps.dependency
+				WHERE deps.position = bp.position AND bp_dep.value = digits.d
+			)
+		GROUP BY 1 having count(digits.d) = 1 order by 1, 2
 	)
-select * from new_board;
+select * from unknowns;
 
